0e7265c2c916fb0ab6e2a3bfc7b64e844d71787f
daichihirano <107654813+daichihirano@users.noreply.github.com>
2022-08-06 00:33:39 +0900

    Productを修正

--- src/page/Product.js
+++ src/page/Product.js
@@ -6,13 +6,17 @@ const Product = (props)=>{
             <h3>
                 作品紹介
             </h3>
-            {props.product.map((product ,key)=> (
+            {props.product.length>0 ? props.product.map((product ,key)=> (
                 <div>
                     <div>作品{key+1}</div>
                     <div id={key}>タイトル : {product.title} </div>
                     <div id={key}>説明概要 : {product.description}</div>
                 </div>
-            ))}
+            )):
+            <div>
+                まだ作品がありません。<br></br>これから頑張ります。
+            </div>
+            }
         </div>
 
     )
